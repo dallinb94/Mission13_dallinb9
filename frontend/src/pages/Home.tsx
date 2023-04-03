651e4abe7af8bf9a5f5d43eb2727b96fe3904dd0
Dallin Blanc <dallinb94@gmail.com>
2023-04-03 14:43:53 -0600

    .

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <>
+      <nav>Home | Podcast | Movie List</nav>
+
+      <h1>Joel Hilton's Movie Collection</h1>
+      <img src="./JoelHiltonHeadshot.jpg" alt="JoelHiltonHeadshot.jpg" />
+    </>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/Podcast.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Podcasts() {
+  return (
+    <>
+      <nav>Home | Podcast | Movie List</nav>
+
+      <h1>Podcasts</h1>
+      <a href="https://baconsale.com">Baconsale Podcast</a>
+    </>
+  );
+}
+
+export default Podcasts;
